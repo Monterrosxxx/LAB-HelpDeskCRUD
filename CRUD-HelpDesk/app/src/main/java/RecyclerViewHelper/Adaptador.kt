e7e68080168d7070b71f27e6975e7c56e2ef0429
Rodrigo Monterrosa <20220377@ricaldone.edu.sv>
2024-06-13 11:17:34 -0600

    Eliminar ya funciona

--- CRUD-HelpDesk/app/src/main/java/RecyclerViewHelper/Adaptador.kt
+++ CRUD-HelpDesk/app/src/main/java/RecyclerViewHelper/Adaptador.kt
@@ -1,12 +1,73 @@
 package RecyclerViewHelper
 
+import android.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import modelo.claseConexion
 import modelo.tbTickets
 import rodrigo.monterrosa.myapplication.R
 
-class Adaptador (var Datos:List<tbTickets>): RecyclerView.Adapter<ViewHolder>() {
+class Adaptador (private var Datos:List<tbTickets>): RecyclerView.Adapter<ViewHolder>() {
+
+    fun actualizarLista(nuevaLista:List<tbTickets>){
+        Datos = nuevaLista
+        notifyDataSetChanged()
+    }
+
+    fun actualizarListaDespuesdeActualizarlosDatos(uuid: String, nuevoNombre: String){
+
+        val index = Datos.indexOfFirst { it.uuid == uuid }
+        Datos[index].tituloTicket == nuevoNombre
+        notifyItemChanged(index)
+    }
+
+    fun eliminarRegistro(nombreTicket: String, position: Int){
+
+        val listaDatos = Datos.toMutableList()
+        listaDatos.removeAt(position)
+
+        GlobalScope.launch(Dispatchers.IO){
+
+            val objConexion = claseConexion().cadenaConexion()
+
+            val deleteProducto = objConexion?.prepareStatement("DELETE tbTickets WHERE tituloTicket = ?")!!
+            deleteProducto.setString(1, nombreTicket)
+            deleteProducto.executeUpdate()
+
+            val commit = objConexion.prepareStatement("commit")!!
+            commit.executeUpdate()
+
+        }
+
+        Datos = listaDatos.toList()
+        notifyItemRemoved(position)
+        notifyDataSetChanged()
+    }
+
+    fun actualizarTicket(nombreTicket: String,uuid: String){
+
+        GlobalScope.launch(Dispatchers.IO){
+            val objConexion = claseConexion().cadenaConexion()
+
+            val actualizarProducto = objConexion?.prepareStatement("UPDATE tbTickets SET tituloTicket = ? WHERE UUID_ticket = ?")!!
+            actualizarProducto.setString(1, nombreTicket)
+            actualizarProducto.setString(2, uuid)
+            actualizarProducto.executeUpdate()
+
+            val commit = objConexion.prepareStatement("commit")!!
+            commit.executeUpdate()
+
+            withContext(Dispatchers.Main){
+                actualizarListaDespuesdeActualizarlosDatos(uuid, nombreTicket)
+            }
+        }
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
         val vista = LayoutInflater.from(parent.context).inflate(R.layout.activity_item_card, parent, false)
@@ -17,8 +78,29 @@ class Adaptador (var Datos:List<tbTickets>): RecyclerView.Adapter<ViewHolder>() 
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
+        val ticket = Datos[position]
+        holder.textView.text = ticket.tituloTicket
+
         val item = Datos[position]
-        holder.txtNombreCard.text = item.tituloTicket
+        holder.imgEliminar.setOnClickListener {
+
+            val context = holder.itemView.context
+            val builder = AlertDialog.Builder(context)
+            builder.setTitle("¿Eliminar?")
+            builder.setMessage("¿Seguro de quererlo eliminar?")
+
+            builder.setPositiveButton("si"){ dialog, wich->
+                eliminarRegistro(item.tituloTicket, position)
+            }
+            builder.setNegativeButton("No"){ dialog, wich ->
+
+            }
+
+            val alertDialog = builder.create()
+            alertDialog.show()
+
+        }
+
     }
 
 
--- CRUD-HelpDesk/app/src/main/java/RecyclerViewHelper/ViewHolder.kt
+++ CRUD-HelpDesk/app/src/main/java/RecyclerViewHelper/ViewHolder.kt
@@ -1,11 +1,17 @@
 package RecyclerViewHelper
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import rodrigo.monterrosa.myapplication.R
 
-class ViewHolder (View: View): RecyclerView.ViewHolder(View) {
+class ViewHolder (View: View): RecyclerView.ViewHolder(View){
 
-    val txtNombreCard = View.findViewById<TextView>(R.id.txtNombreCard)
+    val textView: TextView = View.findViewById(R.id.txtNombreCard)
+    val imgEliminar: ImageView = View.findViewById(R.id.imgEliminar)
+    val imgEditar: ImageView = View.findViewById(R.id.imgEditar)
+
 }
+
+
